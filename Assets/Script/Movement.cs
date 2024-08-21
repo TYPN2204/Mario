95211ba1e890ee4b4b0bf3d89c8388477d15cab7
TYPN2204 <kiraninero@gmail.com>
2024-08-21 14:53:57 +0700

    add velocity adjust

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using UnityEngine;
+
 public class Movement : MonoBehaviour
 {
     public bool moving;
-    public float thrust = 1.5f;
+    private float thrust = 1.5f;
+    private float playerVx = 0f;
+    public float jump = 3f;
+    public float aV = 1f;
+    public float maxVx = 4f;
 
     public GameObject winScreen, loseScreen;
     private Rigidbody2D _rb;
@@ -13,12 +18,28 @@ public class Movement : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        animator.SetBool("isJump",false);
-        animator.SetBool("isTurnRight",true);
+        animator.SetBool("isJump", false);
+        animator.SetBool("isTurnRight", true);
     }
 
     void Update()
     {
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            playerVx += Time.deltaTime * aV;
+        }
+        else
+        {
+            playerVx -= Time.deltaTime * aV;
+        }
+        
+        playerVx = Mathf.Clamp(playerVx, 0, maxVx);
+
         if (moving)
         {
             Move();
@@ -27,112 +48,100 @@ public class Movement : MonoBehaviour
 
     void Move()
     {
-        animator.SetFloat("Speed",Mathf.Abs(_force.x*thrust));
+        animator.SetFloat("Speed", Mathf.Abs(playerVx));
 
         if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
         {
             animator.Play("Jump Left Animation");
-            animator.SetBool("isJump",true);
-            animator.SetBool("isTurnRight",false);
-            _rb.velocity = new Vector2(-1, 1); 
-            thrust = 3;
-            _force = new Vector2(-1, 1);
-            _rb.AddForce(_force * thrust, ForceMode2D.Impulse);
-            thrust = 1.5f;
+            animator.SetBool("isJump", true);
+            animator.SetBool("isTurnRight", false);
+            _rb.velocity = new Vector2(-playerVx, jump); 
             moving = false;
         }
-
         else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
         {
             animator.Play("Jump Right Animation");
-            animator.SetBool("isJump",true);
-            animator.SetBool("isTurnRight",true);
-            _rb.velocity = Vector2.one;
-            thrust = 3;
-            _force = Vector2.one;
-            _rb.AddForce(_force * thrust, ForceMode2D.Impulse);
-            thrust = 1.5f;
+            animator.SetBool("isJump", true);
+            animator.SetBool("isTurnRight", true);
+            _rb.velocity = new Vector2(playerVx, jump);
             moving = false;
         }
-
         else if (Input.GetKey(KeyCode.UpArrow))
         {
-            animator.SetBool("isJump",true);
-            _rb.velocity = Vector2.up;
-            thrust = 3;
-            _force = Vector2.up;
-            _rb.AddForce(_force * thrust, ForceMode2D.Impulse);
-            thrust = 1.5f;
+            animator.SetBool("isJump", true);
+            _rb.velocity = new Vector2(0, jump);
             moving = false;
         }
-
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             animator.Play("Go Left Animation");
-            animator.SetBool("isTurnRight",false);
-            animator.SetBool("isJump",false);
-            _rb.velocity = Vector2.left;
+            animator.SetBool("isTurnRight", false);
+            animator.SetBool("isJump", false);
+            animator.SetBool("isMoving", true);
+            _rb.velocity = new Vector2(-playerVx, 0);
             moving = false;
         }
-
         else if (Input.GetKey(KeyCode.RightArrow))
-        { 
+        {
             animator.Play("Go Right Animation");
-            animator.SetBool("isTurnRight",true);
-            animator.SetBool("isJump",false);
-            _rb.velocity = Vector2.right;
+            animator.SetBool("isTurnRight", true);
+            animator.SetBool("isJump", false);
+            animator.SetBool("isMoving", true);
+            _rb.velocity = new Vector2(playerVx, 0);
             moving = false;
         }
-
         else
         {
-            animator.SetFloat("Speed",0);
-            animator.SetBool("isJump",false);
+            animator.SetBool("isMoving", false);
+            animator.SetBool("isJump", false);
         }
-
     }
+
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            animator.SetBool("isJump",false);
+            animator.SetBool("isJump", false);
             moving = true;
         }
-        else if(collision.gameObject.CompareTag("Ground End")){
+        else if (collision.gameObject.CompareTag("Ground End"))
+        {
             GroundColliderOff[] groundColliders = FindObjectsOfType<GroundColliderOff>();
             foreach (var ground in groundColliders)
             {
                 ground.OffCollider();
             }
             moving = true;
-            thrust = 10;
-            this.GetComponent<Rigidbody2D>().velocity = Vector2.up;
-            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * thrust ,ForceMode2D.Impulse);
+            thrust = 7;
+            _rb.velocity = Vector2.up;
+            _rb.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
             StartCoroutine(Lose());
         }
-        else if(collision.gameObject.CompareTag("Finish")){
+        else if (collision.gameObject.CompareTag("Finish"))
+        {
             moving = false;
             GroundColliderOff[] groundColliders = FindObjectsOfType<GroundColliderOff>();
             foreach (var ground in groundColliders)
             {
                 ground.gameObject.tag = "Untagged";
             }
-            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            _rb.velocity = Vector2.zero;
             thrust = 1;
-            Debug.Log("Add force");
-            this.GetComponent<Rigidbody2D>().AddForce(Vector2.left * thrust ,ForceMode2D.Impulse);
+            _rb.AddForce(Vector2.left * thrust, ForceMode2D.Impulse);
             StartCoroutine(Win());
         }
         else moving = false;
     }
 
-    IEnumerator Win(){
+    IEnumerator Win()
+    {
         yield return new WaitForSeconds(3);
         winScreen.SetActive(true);
         Time.timeScale = 0;
     }
 
-    IEnumerator Lose(){
+    IEnumerator Lose()
+    {
         yield return new WaitForSeconds(3);
         loseScreen.SetActive(true);
         Time.timeScale = 0;
